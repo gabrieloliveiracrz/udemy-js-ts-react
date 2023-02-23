5dc8f6571989305c58803d0401059af50de46e05
Gabriel Ferreira <gabriel.oliveiracrz@gmail.com>
2023-02-22 22:34:03 -0300

    new classes

--- JS/aula_16/index.js
+++ JS/aula_16/index.js
@@ -23,10 +23,34 @@
 // console.log(pessoa1.nome);
 // console.log(pessoa2.nome);
 
-function criaPessoa(nome, sobrenome, idade) {
-    return{
-        nome: nome,
-        sobrenome: sobrenome,
-        idade: idade
+// function criaPessoa(nome, sobrenome, idade) {
+//     return{
+//         nome: nome,
+//         sobrenome: sobrenome,
+//         idade: idade
+//     };
+// }
+
+// const pessoa1 = criaPessoa('Luiz', 'Otávio', 25);
+// const pessoa2 = criaPessoa('Maria', 'Oliveira', 32);
+// const pessoa3 = criaPessoa('João', 'Moreira', 51);
+// const pessoa4 = criaPessoa('Junior', 'Ferreira', 11);
+// const pessoa5 = criaPessoa('Jean', 'Lara', 82);
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25,
+
+    fala(){
+        console.log(`A minha idade atual é ${this.idade}`);
+    },
+
+    incrementaIdade(){
+        this.idade++;
     }
-}
+};
+
+pessoa1.fala();
+pessoa1.incrementaIdade();
+pessoa1.fala();
